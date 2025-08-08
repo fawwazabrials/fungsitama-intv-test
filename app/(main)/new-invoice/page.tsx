95d8d604e7b973fcd8250af4f0eba2ed37acce20
Fawwaz Abrial Saffa <fawwazabrial.fas@gmail.com>
2025-08-08 23:13:50 +0700

    feat: add back button on add invoice page

--- app/(main)/new-invoice/page.tsx
+++ app/(main)/new-invoice/page.tsx
@@ -1,3 +1,4 @@
+import BackButton from '@/components/buttons/back-button';
 import CreateInvoiceForm from '@/components/forms/create-invoice-form';
 import { generateInvoiceNumber } from '@/datasource/internals/generate-invoice-number';
 import React from 'react';
@@ -5,7 +6,12 @@ import React from 'react';
 const CreateInvoicePage = () => {
   return (
     <main className="py-10 px-96">
-      <h1 className="text-4xl font-bold mb-4">Create New Invoice</h1>
+      <h1 className="text-4xl font-bold mb-4">
+        <span>
+          <BackButton />
+        </span>
+        Create New Invoice
+      </h1>
       <CreateInvoiceForm invoiceNumber={generateInvoiceNumber()} />
     </main>
   );
